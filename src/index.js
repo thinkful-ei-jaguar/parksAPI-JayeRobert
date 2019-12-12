14e72e089b830bd1a9096ba8e3b3ba2fdc4eaae5
Alex <robert.perez6@g.austincc.edu>
2019-12-12 00:38:51 -0600

    some cleanup but still need img help

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import './index.css';
 
 
 function displayResults(responseJson,max) {
-  
-  console.log(responseJson);
-  console.log(max);
-  console.log(responseJson.data[1].fullName);
-  console.log(responseJson.data[1].description);
-  console.log(responseJson.data[1].url);
-  
-  
   $('#results-list').empty();
   for (let i = 0; i < max; i++){
   
@@ -20,17 +12,14 @@ function displayResults(responseJson,max) {
       <a href=${responseJson.data[1].url}>Website</a>
       </li>`
     )};
-  //display the results section  <img src='${responseJson.data[i].snippet.thumbnails.default.url}'>
+  //<img src='${responseJson.data[i].snippet.thumbnails.default.url}'>
   $('#results').removeClass('hidden');
 };
 
 function getParks(query, maxResults=10) {
-  //const queryString = formatQueryParams(params)
-
-    //const url = searchURL + '?'+'parks?parkCode=acad'+ apiKey;
+  
     const url2 =`https://developer.nps.gov/api/v1/parks?stateCode=${query}&api_key=D1oQPOGMDAKZOUK9xWfffurbK3iSzL2aGFhz3yDS`
-    //console.log(url);
-    console.log(url2);
+    
   fetch(url2)
     .then(response => {
       if (response.ok) {
@@ -50,7 +39,6 @@ function watchForm() {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    //console.log(searchTerm);
     getParks(searchTerm, maxResults);
   });
 }
